9764ec0e1b04586ff102320ded635031b3b43bea
calogerosorce <calogerosorce00@hotmail.com>
2025-11-12 16:11:37 +0100

    Clean main

--- src/App.jsx
+++ src/App.jsx
@@ -42,49 +42,6 @@ function App() {
     <>
       <Header />
       <Main handleClick={handleClick} man={man} girl={girl} all={all} />
-      {/*  <div className="content">
-        <div className="row g-2 justify-content-center">
-          {
-            man.map(items => (
-              <div key={items.id} className="card col-12 col-md-4 col-lg-2 align-items-center m-2">
-                <img src={items.image} alt="" />
-                <h4>{items.name}</h4>
-                <p>{items.birth_year}</p>
-                <p>{items.nationality}</p>
-                <p>{items.biography}</p>
-                <p>{items.awards.join(", ")}</p>
-              </div>
-            ))
-          }
-
-          {
-            girl.map(items => (
-              <div key={items.id} className="card col-12 col-md-4 col-lg-2 align-items-center m-2">
-                <img src={items.image} alt="" />
-                <h4>{items.name}</h4>
-                <p>{items.birth_year}</p>
-                <p>{items.nationality}</p>
-                <p>{items.biography}</p>
-                <p>{items.awards}</p>
-              </div>
-            ))
-          }
-          {
-            all.map(items => (
-              <div key={items.id} className="card col-12 col-md-4 col-lg-2 align-items-center m-2">
-                <img src={items.image} alt="" />
-                <h4>{items.name}</h4>
-                <p>{items.birth_year}</p>
-                <p>{items.nationality}</p>
-                <p>{items.biography}</p>
-              </div>
-            ))
-          }
-        </div>
-        <button onClick={handleClick} className="btn btn-dark">Press</button> 
-      </div>
-      }*/}
-
 
     </>
   )
