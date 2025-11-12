29a9574784c703874ec3c4f0b4b89582432b7e2c
calogerosorce <calogerosorce00@hotmail.com>
2025-11-12 14:26:10 +0100

    add use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 function App() {
@@ -9,36 +9,37 @@ function App() {
   const [man, setMan] = useState([])
   const [girl, setGirl] = useState([])
   const [all, setAll] = useState([])
-  axios.get(attori)
-    .then(resAttori => {
-      setMan(resAttori.data)
-      /* setAll(resAttori.data) */
 
+  function actor() {
+    axios.get(attori)
+      .then(resAttori => {
+        setMan(resAttori.data)
+      }
+      ).catch(error => {
+        console.error(error)
+      })
 
-    }
-    ).catch(error => {
-      console.error(error)
-    })
-  axios.get(attrici)
-    .then(resAttrici => {
-      setGirl(resAttrici.data)
+    axios.get(attrici)
+      .then(resAttrici => {
+        setGirl(resAttrici.data)
+      }).catch(error => {
+        console.error(error)
+      })
+  }
 
-      /* setAll(resAttrici.data) */
-    }).catch(error => {
-      console.error(error)
-    })
+  useEffect(actor, [])
+
 
 
 
   return (
     <>
       <div className="content">
-        <h1>VI PRESENTIAMO GLI ATTORI</h1>
-        <div className="row">
+        <h1 className="p-4">VI PRESENTIAMO GLI ATTORI</h1>
+        <div className="row g-2 justify-content-center">
           {
             man.map(items => (
-              <div key={items.id} className="card col-12 col-md-3 col-lg-2 align-items-center">
-
+              <div key={items.id} className="card col-12 col-md-4 col-lg-2 align-items-center m-2">
                 <img src={items.image} alt="" />
                 <h4>{items.name}</h4>
                 <p>{items.birth_year}</p>
@@ -51,19 +52,13 @@ function App() {
 
           {
             girl.map(items => (
-              <div key={items.id} className="card col-12 col-md-4 col-lg-4 align-items-center">
-                <div className="card">
-                  <div className="imagine">
-                    <img src={items.image} alt="" />
-                  </div>
-                  <div className="text">
-                    <h4>{items.name}</h4>
-                    <p>{items.birth_year}</p>
-                    <p>{items.nationality}</p>
-                    <p>{items.biography}</p>
-                    <p>{items.awards}</p>
-                  </div>
-                </div>
+              <div key={items.id} className="card col-12 col-md-4 col-lg-2 align-items-center m-2">
+                <img src={items.image} alt="" />
+                <h4>{items.name}</h4>
+                <p>{items.birth_year}</p>
+                <p>{items.nationality}</p>
+                <p>{items.biography}</p>
+                <p>{items.awards}</p>
               </div>
             ))
           }
