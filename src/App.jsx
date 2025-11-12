7e3fe3464ae015f747dd98261b874e5a21d7e195
calogerosorce <calogerosorce00@hotmail.com>
2025-11-12 15:33:54 +0100

    Add logic for button generate all actor in one array

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   const [man, setMan] = useState([])
   const [girl, setGirl] = useState([])
   const [all, setAll] = useState([])
-
   function actor() {
     axios.get(attori)
       .then(resAttori => {
@@ -26,8 +25,13 @@ function App() {
         console.error(error)
       })
   }
-
   useEffect(actor, [])
+  function handleClick() {
+    setAll([...man, ...girl])
+
+  }
+
+
 
 
 
@@ -45,7 +49,7 @@ function App() {
                 <p>{items.birth_year}</p>
                 <p>{items.nationality}</p>
                 <p>{items.biography}</p>
-                <p>{items.awards}</p>
+                <p>{items.awards.join(", ")}</p>
               </div>
             ))
           }
@@ -62,7 +66,19 @@ function App() {
               </div>
             ))
           }
+          {
+            all.map(items => (
+              <div key={items.id} className="card col-12 col-md-4 col-lg-2 align-items-center m-2">
+                <img src={items.image} alt="" />
+                <h4>{items.name}</h4>
+                <p>{items.birth_year}</p>
+                <p>{items.nationality}</p>
+                <p>{items.biography}</p>
+              </div>
+            ))
+          }
         </div>
+        <button onClick={handleClick} className="btn btn-dark">Press</button>
       </div>
 
 
